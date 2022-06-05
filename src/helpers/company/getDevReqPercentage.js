c83bb6e3cf28c1e256f56aa74cce328ee4e86bd9
Christian Velez Medina <christian.gerardo_@outlook.es>
2022-06-05 17:48:46 -0500

    recommendation alghorithm

--- src/helpers/company/getDevReqPercentage.js
+++ src/helpers/company/getDevReqPercentage.js
@@ -14,20 +14,30 @@ export const getDevReqPercentage = (technologies, devStack) => {
       };
    });
 
-   const reqs = techsRequired.length;
+   const reqs = techsRequired.length * 2;
    let reqsMet = 0;
 
 
    for(let i = 0; i < techsRequired.length; i++) {
 
-      const rest = devStack.find(
+      const currentDevTech = devStack.find(
          tech =>
             (tech.technology === techsRequired[i].technology)
       );
 
-      if(rest) {
+      console.log({ currentDevTech, companyTech: techsRequired[i] });
+   
+      // Tiene la tecnologia?
+      if(currentDevTech) {
          reqsMet++;
       }
+   
+      // Cumple con los años de experiencia?
+      if(currentDevTech?.yearsOfExperience >= techsRequired[i]?.yearsOfExperience) {
+         reqsMet++;
+      }
+      
+      
    }
 
    return Math.floor(reqsMet / reqs * 100);
